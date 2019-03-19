b650ef6131949509ad86327a43abbf54bc64f6ff
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-03-19 15:23:18 -0400

    Worked on the media queries, making the website mobile responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,6 @@ var database = firebase.database();
 
 // Initial Values
 var subscribe = "";
-// var email = "";
-// var goalsText = "";
 // Capture Button Click
 $("#subscibe").on("click", function(event) {
   event.preventDefault();
@@ -21,26 +19,9 @@ $("#subscibe").on("click", function(event) {
 
   // Grabbed values from text boxes
   subscribe = $("#subscribe").val().trim();
-  // contact = $("#contact").val().trim();
-  // Code for handling the push
   database.ref().push({
     subscribe: subscribe,
-    // contact: contact,
   });
 });
 
-// $("#contact-submit").on("click", function(event){
-//     console.log("Click");
-//     event.preventDefault();
-
-//     email = $("#email").val().trim();
-//     // helpForm = $("#exampleFormControlSelect1").val();
-//     goalsText = $("#exampleControlTextArea1").val().trim();
-
-//     database.ref().push({
-//         email: email,
-//         // helpForm: helpForm,
-//         goalsText: goalsText,
-//     });
-// });
 
